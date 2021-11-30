9315129541ba0334b02efc90889c451443698825
dolks <dylanoelkers165@gmail.com>
2021-11-30 09:51:17 -0500

    Added climbing animation and functionality on ladders. A collider exists around the background tiles to prevent the camera from moving beyond a certain point. Added cinemacine to camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,20 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D playerRigidbody2D;
     [SerializeField] float runSpeed = 10;
-    [SerializeField] float jumpSpeed = 5;
+    [SerializeField] float jumpSpeed = 25;
+    [SerializeField] float climbSpeed = 25;
     Animator playerAnimator;
     Collider2D playerCollider2D;
+    float playerGravityAtStart;
     const string IS_RUNNING_ANIMATION_PARAM = "isRunning";
+    const string IS_CLIMBING_ANIMATION_PARAM = "isClimbing";
     const string GROUND_LAYER_NAME = "Ground";
+    const string LADDER_LAYER_NAME = "Ladder";
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        playerGravityAtStart = playerRigidbody2D.gravityScale;
         playerAnimator = GetComponent<Animator>();
         playerCollider2D = GetComponent<CapsuleCollider2D>();
     }
@@ -26,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Climb();
     }
 
     private void Run()
@@ -44,7 +50,8 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if (value.isPressed && playerCollider2D.IsTouchingLayers(LayerMask.GetMask(GROUND_LAYER_NAME)))
+        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask(GROUND_LAYER_NAME))) { return; }
+        if (value.isPressed)
         {
             playerRigidbody2D.velocity += new Vector2(0, jumpSpeed);
         }
@@ -56,11 +63,27 @@ public class PlayerMovement : MonoBehaviour
         if (hasHorizontalMovement)
         {
             transform.localScale = new Vector2(Mathf.Sign(playerRigidbody2D.velocity.x), 1f);
-
         } else
         {
             playerAnimator.SetBool(IS_RUNNING_ANIMATION_PARAM, false);
         }
 
+    }
+
+    void Climb()
+    {
+
+        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask(LADDER_LAYER_NAME))) {
+            playerAnimator.SetBool(IS_CLIMBING_ANIMATION_PARAM, false);
+            playerRigidbody2D.gravityScale = playerGravityAtStart;
+            return;
+        }
+        playerRigidbody2D.velocity = new Vector2(
+            playerRigidbody2D.velocity.x,
+            moveInput.y * climbSpeed
+        );
+        playerRigidbody2D.gravityScale = 0;
+        bool hasVerticalMovement = Mathf.Abs(playerRigidbody2D.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool(IS_CLIMBING_ANIMATION_PARAM, hasVerticalMovement);
     }
 }
